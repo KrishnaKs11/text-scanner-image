d92c55b2dc101a131cac086364eac2f732a0d2b5
KrishnaKs11 <krishnabasavarajks@gmail.com>
2024-10-22 13:52:41 +0530

    fix-lint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, {  useState } from 'react';
 import './App.css';
-import FeatureCards from './FeatureCards';
-import Modal from './Modal';
-import { FaPlay, FaCopy, FaDownload } from 'react-icons/fa';
 import HeaderComponent from './HeaderComponent';
 import FooterComponent from './FooterComponent';
 import ProductOverview from './ProductOverview';
 
 function App() {
 
-  const [showModal, setShowModal] = useState(false);
-  const [isPremium, setIsPremium] = useState(false);
 
-
-  const features = [
-    "Convert notes to documents",
-    "Play audio of text/notes",
-    "Upload photo and get the captions and text extracted from images",
-  ];
   return (
     <div className="App">
       <HeaderComponent />
